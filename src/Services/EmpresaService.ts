890cd6698a708c2baa3163ae3d3a738b30f61e8d
Juan Hernandez <juan.martinez3168@gmail.com>
2025-03-22 18:13:31 -0600

    Consulta de empresas correctamente

--- src/Services/EmpresaService.ts
+++ src/Services/EmpresaService.ts
@@ -1,18 +1,6 @@
-import axios from "axios";
+import { Empresa } from "../Models/Empresa";
+import api from "../utils/api";
 
-  export async function consultarEmpresasService(){
-    //await api.get("/empresas");
-
-    //const response = await fetch("http://localhost:8080/empresas", {headers:{Authorization:`Bearer ${localStorage.getItem("token")}`}})
-    //const response = await axios.get("http://localhost:8080/empresas", { headers:{Authorization:`Bearer ${localStorage.getItem("token")}`} })
-    //console.log(await response)
-
-    axios.get("http://localhost:8080/empresas", {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`
-      }
-    })
-    .then(response => console.log(response.data))
-    .catch(error => console.error(error));
-
-  }
+export async function consultarEmpresasService(): Promise<Empresa[]> {
+  return (await api.get("/empresas")).data;
+}
--- src/UI/UIComponents/Navbar.tsx
+++ src/UI/UIComponents/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar: React.FC<NavbarProps> = ({
 
   useEffect(() => {
     async function buscar() {
-      await consultarEmpresasService()
+      setEmpresas(await consultarEmpresasService());
     }
     buscar();
   }, []);
